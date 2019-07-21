8c93f6770d0dd7e79f8ccac948efca10454d5ba7
opsbible <34233121+faster-snail@users.noreply.github.com>
2019-07-21 21:53:32 +0800

    Add files via upload

--- utils/gpwd.go
+++ utils/gpwd.go
@@ -1,2 +1,21 @@
 package utils
 
+import (
+	"encoding/hex"
+	"crypto/md5"
+)
+
+//Md5encode 密码md5加密
+func Md5encode (key string) string {
+	hash := md5.New()
+	hash.Write([]byte(key))
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+//Md5Compare 密码校验，比对结果
+func Md5Compare (str string,key string) bool {
+	if Md5encode(str) == key {
+		return true
+	}
+	return false
+}
